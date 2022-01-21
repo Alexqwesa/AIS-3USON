9cdf917cc7b171192b61a2a11dcc0fbb93e85cce
Savin Alexander Viktorovich <alexo.veto@gmail.com>
2022-01-21 20:35:15 +0300

    sql fix typo

--- sql/schema.sql
+++ sql/schema.sql
@@ -4114,7 +4114,7 @@ CREATE TABLE `user_change` (
   `cr_by` int DEFAULT NULL,
   `upd_by` int DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `user_changer_id_IDX` (`id`) USING BTREE
+  UNIQUE KEY `user_change_id_IDX` (`id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
